bd2a66f87c0f63fcded4fff89dd77a001f68014e
cinecode <u2504041@rajagiri.edu.in>
2026-09-14 19:44:09 +0530

    Add ChallengeResult class with scoring attributes

--- scoring/ChallengeResult.java
+++ scoring/ChallengeResult.java
@@ -1,1 +1,18 @@
 
+package scoring;
+
+public class ChallengeResult {
+    private final double percentileScore;      // 0 to 100
+    private final double difficultyScore;      // 0.0 to 1.0
+    private final double verificationMultiplier; // 0.3 to 1.2
+
+    public ChallengeResult(double percentileScore, double difficultyScore, double verificationMultiplier) {
+        this.percentileScore = percentileScore;
+        this.difficultyScore = difficultyScore;
+        this.verificationMultiplier = verificationMultiplier;
+    }
+
+    public double getPercentileScore() { return percentileScore; }
+    public double getDifficultyScore() { return difficultyScore; }
+    public double getVerificationMultiplier() { return verificationMultiplier; }
+}
